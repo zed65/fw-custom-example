0360da8981b83c241de7dac4714698b070a1bf86
zed65 <zed65@hotmail.com>
2025-07-17 21:06:31 +0200

    Update board_configuration.cpp

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 
 Gpio getCommsLedPin() {
-	return Gpio::Unassigned;
+	return Gpio::F15;
 }
 
 Gpio getRunningLedPin() {
@@ -12,33 +12,61 @@ Gpio getWarningLedPin() {
 	return Gpio::Unassigned;
 }
 
-// board-specific configuration setup
 void setBoardDefaultConfiguration() {
-    // engineConfiguration->injectionPins[0] = Gpio::F13;
-    // engineConfiguration->ignitionPins[0] = Gpio::E15;
-
-//   engineConfiguration->triggerInputPins[0] = Gpio::B1;
-//	engineConfiguration->triggerInputPins[1] = Gpio::Unassigned;
-
-//	engineConfiguration->map.sensor.hwChannel = EFI_ADC_3;
-
-//	engineConfiguration->clt.adcChannel = EFI_ADC_1;
-
-//	engineConfiguration->iat.adcChannel = EFI_ADC_2;
-
-
-    	// 5.6k high side/10k low side = 1.56 ratio divider
-  //  	engineConfiguration->analogInputDividerCoefficient = 1.56f;
-
-    	// 6.34k high side/ 1k low side
-//    	engineConfiguration->vbattDividerCoeff = (6.34 + 1) / 1;
-
-//	engineConfiguration->adcVcc = 3.3f;
-
-//	engineConfiguration->clt.config.bias_resistor = 2490;
-//	engineConfiguration->iat.config.bias_resistor = 2490;
-
-
-	// Battery sense on PA0
-//	engineConfiguration->vbattAdcChannel = EFI_ADC_0;
+	engineConfiguration->vbattDividerCoeff = 9.2f;
+	engineConfiguration->vbattAdcChannel = EFI_ADC_15;
 }
+
+static Gpio OUTPUTS[] = {
+    Gpio::D11, // 
+    Gpio::D12, // 
+    Gpio::D13, //
+    Gpio::D14, //
+    Gpio::E11, //
+    Gpio::E12,
+};
+
+int getBoardMetaOutputsCount() {
+    return efi::size(OUTPUTS);
+}
+
+Gpio* getBoardMetaOutputs() {
+    return OUTPUTS;
+}
+
+void setCustomVbatt() {
+	// set vbatt_divider 5.835
+	// 33k / 6.8k
+	engineConfiguration->vbattDividerCoeff = 9.2; // 5.835
+
+	engineConfiguration->vbattAdcChannel = EFI_ADC_15;
+
+}
+/*
+static void setInjectorPins() {
+	engineConfiguration->injectionPins[0] = Gpio::E0;
+	engineConfiguration->injectionPins[1] = Gpio::E6;
+	engineConfiguration->injectionPins[2] = Gpio::D9;
+	engineConfiguration->injectionPins[3] = Gpio::E3;
+}
+
+static void setIgnitionPins() {
+	engineConfiguration->ignitionPins[0] = Gpio::A8;
+	engineConfiguration->ignitionPins[1] = Gpio::C9;
+	engineConfiguration->ignitionPins[2] = Gpio::A10;
+	engineConfiguration->ignitionPins[3] = Gpio::C8;
+}
+
+void setBoardDefaultConfiguration() {
+	engineConfiguration->map.sensor.hwChannel = EFI_ADC_0;
+	engineConfiguration->clt.adcChannel = EFI_ADC_1;
+	engineConfiguration->iat.adcChannel = EFI_ADC_13;
+	engineConfiguration->afr.hwChannel = EFI_ADC_14;
+
+	engineConfiguration->vbattAdcChannel = EFI_ADC_12;
+	engineConfiguration->analogInputDividerCoefficient = 10.2f;
+	
+	engineConfiguration->triggerInputPins[0] = Gpio::B1;
+	engineConfiguration->triggerInputPins[1] = Gpio::A6;
+}
+*/
