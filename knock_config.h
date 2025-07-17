1d73449a547af731a7486941838ab1054ea97d77
zed65 <zed65@hotmail.com>
2025-07-17 21:05:46 +0200

    Update knock_config.h

--- knock_config.h
+++ knock_config.h
@@ -7,9 +7,15 @@
 // Knock is on ADC3
 #define KNOCK_ADC ADCD3
 
-// knock 1 - pin PA3
-#define KNOCK_ADC_CH1 ADC_CHANNEL_IN3
-#define KNOCK_PIN_CH1 Gpio::A3
+//RusZed31:
+// knock 1 - pin PF4
+#define KNOCK_ADC_CH1 ADC_CHANNEL_IN14
+#define KNOCK_PIN_CH1 Gpio::F4
+
+// knock 2 - pin PF5
+#define KNOCK_HAS_CH2 true
+#define KNOCK_ADC_CH2 ADC_CHANNEL_IN15
+#define KNOCK_PIN_CH2 Gpio::F5
 
 // Sample rate & time - depends on the exact MCU
 #define KNOCK_SAMPLE_TIME ADC_SAMPLE_84
