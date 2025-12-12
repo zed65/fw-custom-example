d82e762ba56fc77431031150d45e8827855dcf25
zed65 <zed65@hotmail.com>
2025-12-12 12:30:46 +0100

    Update board.mk with new definitions and comments

--- board.mk
+++ board.mk
@@ -1,21 +1,35 @@
-include $(BOARD_DIR)/firmware/firmware.mk
 
+# List of all the board related files.
+BOARDCPPSRC = $(BOARD_DIR)/board_configuration.cpp
+DDEFS += -DEFI_MAX_31855=TRUE
 
-BOARDINC += $(BOARD_DIR)/generated/controllers/generated
+# This board has some Accelerometer onboard
+DDEFS += -DEFI_ONBOARD_MEMS=TRUE
+
+# And some custom board have LIS2DW12TR
+EFI_ONBOARD_MEMS_LIS2DW12 = yes
+DDEFS += -DLIS2DW12_USE_SPI=TRUE -DLIS2DW12_SHARED_SPI=TRUE
 
 # defines SHORT_BOARD_NAME
 include $(BOARD_DIR)/meta-info.env
 
-# this would save some flash while being unable to update WBO controller firmware
-DDEFS += -DEFI_WIDEBAND_FIRMWARE_UPDATE=FALSE
+# one day when we are grown ups and can coordinate a real life test we shall revisit https://github.com/rusefi/rusefi/issues/6008
+DDEFS += -DDISABLE_PIN_STATE_VALIDATION=TRUE
 
-# assign critical LED to a non-existent pin if you do not have it on your board
-# good old PD14 is still the default value
-# DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
+# reduce memory usage monitoring
+DDEFS += -DRAM_UNUSED_SIZE=100
 
-# EGT chip
-#un-comment to enable
-#DDEFS += -DEFI_MAX_31855=TRUE
+# assign critical LED to a non-existent pin
+DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
 
-#see main repo for details on this any many other optional subsystems. We have too many, one has to choose what fits into his choice of stm32
-#DDEFS += -DEFI_ONBOARD_MEMS=TRUE
+# MAJOR HACK? SOMETHING IS FUNNY HERE?!
+# DDEFS += -DDISABLE_PIN_STATE_VALIDATION=TRUE
+
+# temporary or not? (pps noise stuff)
+# DDEFS += -DETB_INTERMITTENT_LIMIT=60001
+
+DDEFS += -DSTM32_ADC_USE_ADC3=TRUE
+# todo: make knock pin software-selectable?
+# todo: DDEFS += -DEFI_SOFTWARE_KNOCK=TRUE
+
+DDEFS += -DEFI_SOFTWARE_KNOCK=TRUE -DSTM32_ADC_USE_ADC3=TRUE
