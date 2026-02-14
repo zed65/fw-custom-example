bec65c21c831edeaf29f7793d1bfd7a6e5193717
GitHub git update Action <action@github.com>
2026-02-14 01:18:16 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_ruszed.h
+++ generated/controllers/generated/signature_ruszed.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 162660972
-#define TS_SIGNATURE "rusEFI main.2026.02.13.ruszed.162660972"
+#define SIGNATURE_HASH 793113917
+#define TS_SIGNATURE "rusEFI main.2026.02.14.ruszed.793113917"
